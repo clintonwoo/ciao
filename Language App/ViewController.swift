596e5437798281cbd6a288b3a6f31c0d27298db9
Clinton D'Annolfo <clintondannolfo@Clintons-MBP.lan>
2014-12-06 17:34:45 +0800

    Initial Commit

---
+++ Language App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Language App
+//
+//  Created by Clinton D'Annolfo on 6/12/2014.
+//  Copyright (c) 2014 Clinton D'Annolfo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
